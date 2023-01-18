d569943c667346de6fe9f604c02e7974dc5b9d94
Synoxal <536662@chrome.escg.ac.uk>
2023-01-18 10:14:13 +0000

    no message

--- Full Project/Assets/GameManager.cs
+++ Full Project/Assets/GameManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject CorrectPlatformPrefab;
+    public GameObject FalsePlatformPrefab;
+    public Transform Clouds;
+
+    public int platformLayerCount;
+
+    int a;
+
+
     void Start()
     {
-        
-    }
+        Vector3 correctSpawnPosition = new Vector3();
+        correctSpawnPosition.y = -1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 falseSpawnPositionB = new Vector3();
+        falseSpawnPositionB.y = -1;
+
+        Vector3 falseSpawnPositionC = new Vector3();
+        falseSpawnPositionC.y = -1;
+
+
+        for (int i = 0; i < platformLayerCount; i++)
+        {
+            correctSpawnPosition.y = correctSpawnPosition.y + 5;
+            falseSpawnPositionB.y = falseSpawnPositionB.y + 5;
+            falseSpawnPositionC.y = falseSpawnPositionC.y + 5;
+            a = Random.Range(0, 2);
+
+            if (a == 0)
+            {
+                correctSpawnPosition.x = 1.75f;
+                falseSpawnPositionB.x = 3.5f;
+                falseSpawnPositionC.x = 5.25f;
+            }
+            else if (a == 1)
+            {
+                correctSpawnPosition.x = 3.5f;
+                falseSpawnPositionB.x = 1.75f;
+                falseSpawnPositionC.x = 5.25f;
+            }
+            else if (a == 2)
+            {
+                correctSpawnPosition.x = 5.25f;
+                falseSpawnPositionB.x = 1.75f;
+                falseSpawnPositionC.x = 3.5f;
+            }
+            GameObject NextPlatformA = Instantiate(CorrectPlatformPrefab, correctSpawnPosition, Quaternion.identity, Clouds);
+            GameObject NextPlatformB = Instantiate(FalsePlatformPrefab, falseSpawnPositionB, Quaternion.identity, Clouds);
+            GameObject NextPlatformC = Instantiate(FalsePlatformPrefab, falseSpawnPositionC, Quaternion.identity, Clouds);
+
+
+        }
     }
 }
