d3865d6e3517ce03b07529123bcab5c3b34ff7a7
Synoxal <536662@chrome.escg.ac.uk>
2023-01-25 08:42:50 +0000

    Dad commit

--- Full Project/Assets/GameManager.cs
+++ Full Project/Assets/GameManager.cs
@@ -1,70 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Prefabs")]
     public GameObject CorrectPlatformPrefab;
     public GameObject FalsePlatformPrefab;
     public Transform Clouds;
 
+    [Header("Misc")]
     public int platformLayerCount;
 
     int plusLayerCount;
 
-    bool PlatformUpdate;
+    int a;
 
     GameObject NextPlatformA;
     GameObject NextPlatformB;
     GameObject NextPlatformC;
 
+    GameObject CurrentPlatformA;
+    GameObject CurrentPlatformB;
+    GameObject CurrentPlatformC;
+
+    [Header("First Platforms")]
+    public GameObject FirstPlatformA;
+    public GameObject FirstPlatformB;
+    public GameObject FirstPlatformC;
+
+    GameObject LastPlatformA;
+    GameObject LastPlatformB;
+    GameObject LastPlatformC;
 
     Vector3 correctSpawnPosition = new Vector3();
-
     Vector3 falseSpawnPositionB = new Vector3();
-
     Vector3 falseSpawnPositionC = new Vector3();
-
-
-    int a;
-
 
     void Start()
     {
-        PlatformUpdate = false;
-
         correctSpawnPosition.y = -3;
-
-
         falseSpawnPositionB.y = -3;
-
-
         falseSpawnPositionC.y = -3;
+        plusLayerCount = platformLayerCount + 1;
 
-
-        plusLayerCount = platformLayerCount + 2;
+        CurrentPlatformA = FirstPlatformA;
+        CurrentPlatformB = FirstPlatformB;
+        CurrentPlatformB = FirstPlatformC;
     }
 
 
     private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PlatformUpdate = true;
-            if (PlatformUpdate == true)
-            {
-                GameObject CurrentPlatformA = NextPlatformA;
-                GameObject CurrentPlatformB = NextPlatformB;
-                GameObject CurrentPlatformC = NextPlatformC;
-                platformLayerCount++;
-                plusLayerCount++;
-                Debug.Log(platformLayerCount);
-            }
-        }
-
-
-        
-        if (platformLayerCount + 1 == plusLayerCount)
+    {     
+        if (platformLayerCount < plusLayerCount)
         {
             correctSpawnPosition.y = correctSpawnPosition.y + 5;
             falseSpawnPositionB.y = falseSpawnPositionB.y + 5;
@@ -89,12 +78,37 @@ public class GameManager : MonoBehaviour
                 falseSpawnPositionB.x = 1.75f;
                 falseSpawnPositionC.x = 3.5f;
             }
+            // Creates a layer of new clouds.
             NextPlatformA = Instantiate(CorrectPlatformPrefab, correctSpawnPosition, Quaternion.identity, Clouds);
             NextPlatformB = Instantiate(FalsePlatformPrefab, falseSpawnPositionB, Quaternion.identity, Clouds);
             NextPlatformC = Instantiate(FalsePlatformPrefab, falseSpawnPositionC, Quaternion.identity, Clouds);
-
-
-
+            Debug.Log("New Clouds Created");
+            platformLayerCount++;
+            Debug.Log("PlatLayerCount increased to: " + platformLayerCount);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(waiter());
+        }
+    }
+    IEnumerator waiter()
+    {
+
+        plusLayerCount++;
+        Debug.Log("PlusLayerCount increased to: " + plusLayerCount);
+        
+        LastPlatformA = CurrentPlatformA;
+        LastPlatformB = CurrentPlatformB;
+        LastPlatformC = CurrentPlatformC;
+
+        yield return new WaitForSeconds(3);
+        
+        CurrentPlatformA = NextPlatformA;
+        CurrentPlatformB = NextPlatformB;
+        CurrentPlatformC = NextPlatformC;
+
+        Destroy(LastPlatformA);
+        Destroy(LastPlatformB);
+        Destroy(LastPlatformC);
     }
 }
